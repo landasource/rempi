b1b91b756267eb81f06fc87d129bcc371ea0c0d9
Zsolt Lengyel <zsolt.lengyel.it@gmail.com>
2014-11-14 18:12:45 +0100

    Design site

--- rempi-server/src/main/resources/webapp/static/js/app/clients.js
+++ rempi-server/src/main/resources/webapp/static/js/app/clients.js
@@ -1,52 +1,62 @@
-
 var wsurl = "ws://localhost:1234/ws/rempi";
-var app = angular.module('rempi-server', [ 'angular-websocket' ]).config(function(WebSocketProvider) {
-    WebSocketProvider.prefix('').uri(wsurl);
+var app = angular.module('rempi-server', [ 'angular-websocket' ]).config(
+		function(WebSocketProvider) {
+			WebSocketProvider.prefix('').uri(wsurl);
+		});
+
+app.filter('empty', function() {
+	return function(input) {		
+		return jQuery.isEmptyObject(input);
+	};
 });
 
 app.controller('ClientsCtrl', function($scope, WebSocket, $http) {
 
-    $scope.clients = {};
+	$scope.clients = {};
 
-    WebSocket.onopen(function() {
-        console.log('connection');
-    });
+	WebSocket.onopen(function() {
+		console.log('connection');
+	});
 
-    WebSocket.onmessage(function(event) {
-    	console.log("WS event happened", event);
-    	
-        var data = JSON.parse(event.data);
-        switch (data.eventType) {
+	WebSocket.onmessage(function(event) {
+		console.log("WS event happened", event);
 
-        case "connected":
-            $scope.clients[data.clientId] = {};
-            break;
+		var data = JSON.parse(event.data);
+		switch (data.eventType) {
 
-        case "disconnected":
-            delete $scope.clients[data.clientId];
-            break;
-        }
-    });
-    
-    $scope.disconnet = function(clientId){
-        
-        $http.get('/stopClient?clientId=' + clientId);
-        
-    }
+		case "connected":
+			$scope.clients[data.clientId] = {};
+			break;
 
-    $http.get('/client/json').success(function(data, status, headers, config) {
-        $scope.clients = data;
-    }).error(function(data, status, headers, config) {
-        // log error
-    });
+		case "disconnected":
+			delete $scope.clients[data.clientId];
+			break;
+		}
+	});
 
-    $scope.capture = function(clientId, evt) {
+	$scope.disconnet = function(clientId) {
 
-        var btn = evt.target;
+		$http.get('/stopClient?clientId=' + clientId);
 
-        var img = $('<img>').attr('src', '/rempi-server/client/capture?clientId=' + clientId).load(function() {
-            $(btn).next('div').html(this);
-        });
-    }
+	}
+
+	$http.get('/client/json').success(function(data, status, headers, config) {
+		$scope.clients = data;
+	}).error(function(data, status, headers, config) {
+		// log error
+	});
+
+	$scope.capture = function(clientId, evt) {
+
+		var btn = evt.target;
+
+		var img = $('<img>').attr('src',
+				'/rempi-server/client/capture?clientId=' + clientId).load(
+				function() {
+					$(btn).next('div').html(this);
+				});
+	}
 
 });
+
+$('.ui.dropdown').dropdown('show');
